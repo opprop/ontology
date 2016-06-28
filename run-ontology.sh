a518a5e4fd719327baef77abecfc55dbf4f14c7c
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-06-28 17:19:15 -0400

    add run-ontology.sh

---
+++ run-ontology.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ROOT=$(cd ../ && pwd)
+
+CFI="$ROOT"/checker-framework-inference
+
+CF="$ROOT"/checker-framework
+
+SOLVER=ontology.solvers.classic.OntologySolver
+
+DEBUG_SOVLER=checkers.inference.solver.DebugSolver
+
+IS_HACK=true
+
+CHECKER=ontology.OntologyChecker
+
+# SOLVER="$DEBUG_SOVLER"
+# IS_HACK=false
+
+CFI_JARS="$CFI"/dist/checker.jar:"$CFI"/dist/plume.jar:"$CFI"/dist/checker-framework-inference.jar
+
+export CLASSPATH="$ROOT"/ontology/bin:"$ROOT"/generic-type-inference-solver/bin
+
+$CFI/scripts/inference-dev --checker "$CHECKER" --solver "$SOLVER" --hacks="$IS_HACK" -m INFER "$@"
