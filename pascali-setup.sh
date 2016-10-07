0acf19b8e1ad1746d868479d4dae79c8f6558aee
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-10-07 19:21:35 -0400

    back to opprop

--- pascali-setup.sh
+++ pascali-setup.sh
@@ -8,10 +8,10 @@ export SHELLOPTS
 export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
 
 #TODO: avoid hard coding. maybe get repo from TRAVIS_SLUG
-export REPO_SITE=pascaliUWat
+export REPO_SITE=opprop
 
 CUR_DIR=$(pwd)
-JSR308=$(cd ../ && pwd)
+export JSR308=$(cd ../ && pwd)
 
 ##### build checker-framework
 if [ -d $JSR308/checker-framework ] ; then
