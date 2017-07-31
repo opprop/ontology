d8b605558feb3be017bc8618de299b10615a13bb
CharlesZ-Chen <charleszhuochen@gmail.com>
2017-07-31 16:10:12 -0400

    Statistic infer result in JaCop solver.

--- src/ontology/solvers/backend/jacop/OntologyJaCopSolver.java
+++ src/ontology/solvers/backend/jacop/OntologyJaCopSolver.java
@@ -4,15 +4,20 @@ import java.util.Collection;
 import java.util.Map;
 
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.AnnotationMirror;
 
 import org.checkerframework.framework.type.QualifierHierarchy;
 
+import checkers.inference.DefaultInferenceSolution;
+import checkers.inference.InferenceSolution;
 import checkers.inference.model.Constraint;
 import checkers.inference.model.Serializer;
 import checkers.inference.model.Slot;
 import constraintsolver.BackEnd;
 import constraintsolver.ConstraintSolver;
 import constraintsolver.Lattice;
+import ontology.util.OntologyStatisticUtil;
+import util.PrintUtils;
 
 public class OntologyJaCopSolver extends ConstraintSolver {
 
@@ -22,6 +27,14 @@ public class OntologyJaCopSolver extends ConstraintSolver {
             ProcessingEnvironment processingEnvironment, Lattice lattice, Serializer<?, ?> defaultSerializer) {
         return new OntologyJaCopBackend(configuration, slots, constraints, qualHierarchy, processingEnvironment,
                 null, lattice);
+    }
+
+    @Override
+    protected InferenceSolution solve() {
+        Map<Integer, AnnotationMirror> result = realBackEnd.solve();
+        PrintUtils.printResult(result);
+        OntologyStatisticUtil.writeInferenceResult("ontology-infer-result.txt", result);
+        return new DefaultInferenceSolution(result);
     }
 
     @Override
