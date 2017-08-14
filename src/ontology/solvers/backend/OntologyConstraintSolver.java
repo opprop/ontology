bd9feb0b7e52f5296e3f86f96ade924569bac997
CharlesZ-Chen <charleszhuochen@gmail.com>
2017-08-14 14:36:28 -0400

    Adapt change of integrating GTIS into CFI.

--- src/ontology/solvers/backend/OntologyConstraintSolver.java
+++ src/ontology/solvers/backend/OntologyConstraintSolver.java
@@ -2,7 +2,6 @@ package ontology.solvers.backend;
 
 import ontology.qual.OntologyValue;
 import ontology.util.OntologyStatisticUtil;
-import ontology.util.ViolatedConsDiagnostic;
 import ontology.util.OntologyUtils;
 
 import org.checkerframework.framework.type.QualifierHierarchy;
@@ -34,16 +33,15 @@ import checkers.inference.model.Serializer;
 import checkers.inference.model.Slot;
 import checkers.inference.model.SubtypeConstraint;
 import checkers.inference.model.VariableSlot;
-import constraintgraph.ConstraintGraph;
-import constraintgraph.GraphBuilder;
-import constraintgraph.GraphBuilder.SubtypeDirection;
-import constraintgraph.Vertex;
-import constraintsolver.BackEnd;
-import constraintsolver.ConstraintSolver;
-import constraintsolver.Lattice;
-import constraintsolver.TwoQualifiersLattice;
+import checkers.inference.solver.GeneralSolver;
+import checkers.inference.solver.backend.BackEnd;
+import checkers.inference.solver.backend.BackEndType;
+import checkers.inference.solver.constraintgraph.ConstraintGraph;
+import checkers.inference.solver.constraintgraph.Vertex;
+import checkers.inference.solver.frontend.Lattice;
+import checkers.inference.solver.frontend.TwoQualifiersLattice;
 
-public class OntologyConstraintSolver extends ConstraintSolver {
+public class OntologyConstraintSolver extends GeneralSolver {
 
     // TODO: why this processingEnv does not initialted in Constructor?
     private ProcessingEnvironment processingEnvironment;
@@ -73,7 +71,7 @@ public class OntologyConstraintSolver extends ConstraintSolver {
             }
 
             AnnotationMirror CUR_ONTOLOGY_BOTTOM = OntologyUtils.createOntologyAnnotationByValues(processingEnvironment, ontologyValues);
-            TwoQualifiersLattice latticeFor2 = configureLatticeFor2(OntologyUtils.ONTOLOGY_TOP, CUR_ONTOLOGY_BOTTOM);
+            TwoQualifiersLattice latticeFor2 = createTwoQualifierLattice(OntologyUtils.ONTOLOGY_TOP, CUR_ONTOLOGY_BOTTOM);
 
             Set<Constraint> consSet = entry.getValue();
             Slot vertixSlot = entry.getKey().getSlot();
@@ -104,7 +102,7 @@ public class OntologyConstraintSolver extends ConstraintSolver {
         InferenceSolution mergedSolution = mergeSolution(inferenceSolutionMaps);
 
         try {
-            verifyMergedSolution(mergedSolution, constraints, qualHierarchy, inferenceSolutionMaps);
+            OntologyStatisticUtil.verifySolution(mergedSolution, constraints, qualHierarchy, inferenceSolutionMaps);
         } finally {
             if (collectStatistic) {
                 OntologyStatisticUtil.getPostVerifyStatRecorder().writeStatistic();
@@ -114,112 +112,6 @@ public class OntologyConstraintSolver extends ConstraintSolver {
         return mergedSolution;
     }
 
-    /**
-     * verify the merged solution whether is consistent with constraints
-     * @param mergedSolution
-     * @param constraints
-     * @param qualifierHierarchy
-     */
-    protected void verifyMergedSolution(InferenceSolution mergedSolution,
-            Collection<Constraint> constraints, QualifierHierarchy qualifierHierarchy, List<Map<Integer, AnnotationMirror>> solutionMaps) {
-        List<ViolatedConsDiagnostic> diagnosticList = new ArrayList<>();
-        for (Constraint constraint : constraints) {
-            //TODO: also verify other kinds of constraint
-            // currently ontology use ExistentialConstraint and EqualityConstraint
-            if (!(constraint instanceof SubtypeConstraint)) {
-                continue;
-            }
-            SubtypeConstraint sConstraint = (SubtypeConstraint) constraint;
-            Slot subtypeSlot = sConstraint.getSubtype();
-            Slot supertypeSlot = sConstraint.getSupertype();
-            AnnotationMirror subtype, supertype;
-
-            // currently both suptypeSlot and supertypeSlot should be type of VariableSlot
-            assert subtypeSlot instanceof VariableSlot && supertypeSlot instanceof VariableSlot;
-
-            final int subtypeId = ((VariableSlot) subtypeSlot).getId();
-            final int supertypeId = ((VariableSlot) supertypeSlot).getId();
-
-            if (subtypeSlot instanceof ConstantSlot && supertypeSlot instanceof ConstantSlot) {
-                continue;
-
-            } else if (subtypeSlot instanceof ConstantSlot) {
-                subtype = ((ConstantSlot) subtypeSlot).getValue();
-                supertype= mergedSolution.getAnnotation(supertypeId);
-
-                assert subtype != null;
-
-                if (supertype == null) {
-                    logNoSolution(sConstraint, subtype, supertype);
-                    continue;
-                }
-
-            } else if (supertypeSlot instanceof ConstantSlot) {
-                subtype = mergedSolution.getAnnotation(subtypeId);
-                supertype = ((ConstantSlot) supertypeSlot).getValue();
-
-                assert supertype != null;
-
-                if (subtype == null) {
-                    logNoSolution(sConstraint, subtype, supertype);
-                    continue;
-                }
-
-            } else {
-                subtype = mergedSolution.getAnnotation(subtypeId);
-                supertype = mergedSolution.getAnnotation(supertypeId);
-
-                if (subtype == null || supertype == null) {
-                    logNoSolution(sConstraint, subtype, supertype);
-                    continue;
-                }
-            }
-
-            assert subtype != null && supertype != null;
-
-            //avoid checking poly ontology
-            if (AnnotationUtils.areSame(OntologyUtils.POLY_ONTOLOGY, subtype)
-                    || AnnotationUtils.areSame(OntologyUtils.POLY_ONTOLOGY, supertype)) {
-                continue;
-            }
-
-            if (!qualifierHierarchy.isSubtype(subtype, supertype)) {
-                ViolatedConsDiagnostic consDiagRes = new ViolatedConsDiagnostic(sConstraint, subtype, supertype);
-
-                List<AnnotationMirror> subtypeSolutions = new ArrayList<> ();
-                List<AnnotationMirror> supertypeSolutions = new ArrayList<> ();
-
-                for (Map<Integer, AnnotationMirror> solutionMap : solutionMaps) {
-                    if (solutionMap.containsKey(subtypeId)) {
-                        subtypeSolutions.add(solutionMap.get(subtypeId));
-                    }
-                    if (solutionMap.containsKey(supertypeId)) {
-                        supertypeSolutions.add(solutionMap.get(supertypeId));
-                    }
-                }
-
-                consDiagRes.setSubtypeSolutions(subtypeSolutions);
-                consDiagRes.setSupertypeSolutions(supertypeSolutions);
-                diagnosticList.add(consDiagRes);
-                OntologyStatisticUtil.getPostVerifyStatRecorder().recordViolatedConsDiags(consDiagRes);
-            }
-        }
-
-        if (!diagnosticList.isEmpty()) {
-            StringBuilder sBuilder = new StringBuilder();
-            sBuilder.append("solved solution doesn't consistent with below constraints: \n");
-            for (ViolatedConsDiagnostic result : diagnosticList) {
-                sBuilder.append(result + "\n");
-            }
-            ErrorReporter.errorAbort(sBuilder.toString());
-        }
-    }
-
-    private void logNoSolution(SubtypeConstraint subtypeConstraint, AnnotationMirror subtype, AnnotationMirror supertype) {
-        InferenceMain.getInstance().logger.warning("no solution for subtype constraint: " + subtypeConstraint +
-                "\tinferred subtype: " + subtype + "\tinferred supertype: " + supertype);
-        OntologyStatisticUtil.getPostVerifyStatRecorder().recordMissingSolution(subtypeConstraint);
-    }
 
     private void addPreferenceToCurBottom(ConstantSlot curBtm, Set<Constraint> consSet) {
         Set<Constraint> preferSet = new HashSet<>();
@@ -239,8 +131,16 @@ public class OntologyConstraintSolver extends ConstraintSolver {
         consSet.addAll(preferSet);
     }
     @Override
-    protected Serializer<?, ?> createSerializer(String value, Lattice lattice) {
-        return new OntologyConstraintSerializer<>(value, lattice);
+    protected Serializer<?, ?> createSerializer(BackEndType backEndType, Lattice lattice) {
+        switch (backEndType) {
+        case MAXSAT:
+        case LINGELING: {
+            return new OntologyMaxsatSerializer(lattice);
+        }
+
+        default:
+            return backEndType.createDefaultSerializer(lattice);
+        }
     }
 
     @Override
@@ -280,13 +180,6 @@ public class OntologyConstraintSolver extends ConstraintSolver {
         }
 
         return new DefaultInferenceSolution(result);
-    }
-
-    @Override
-    protected ConstraintGraph generateGraph(Collection<Slot> slots, Collection<Constraint> constraints) {
-        GraphBuilder graphBuilder = new GraphBuilder(slots, constraints, SubtypeDirection.FROMSUBTYPE);
-        ConstraintGraph constraintGraph = graphBuilder.buildGraph();
-        return constraintGraph;
     }
 
     @Override
