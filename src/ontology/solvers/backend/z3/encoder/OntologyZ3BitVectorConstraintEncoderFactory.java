8f3fa44383633e0ed6de70ccfd289168a063cf42
Mier Ta <topnessman@sina.com>
2018-04-11 22:36:31 -0400

    Update according to upstream changes

--- src/ontology/solvers/backend/z3/encoder/OntologyZ3BitVectorConstraintEncoderFactory.java
+++ src/ontology/solvers/backend/z3/encoder/OntologyZ3BitVectorConstraintEncoderFactory.java
@@ -1,24 +1,22 @@
 package ontology.solvers.backend.z3.encoder;
 
+import com.microsoft.z3.Context;
+
 import checkers.inference.solver.backend.z3.Z3BitVectorFormatTranslator;
 import checkers.inference.solver.backend.z3.encoder.Z3BitVectorConstraintEncoderFactory;
 import checkers.inference.solver.backend.z3.encoder.Z3BitVectorSubtypeConstraintEncoder;
 import checkers.inference.solver.frontend.Lattice;
-import com.microsoft.z3.Context;
 
 public class OntologyZ3BitVectorConstraintEncoderFactory
         extends Z3BitVectorConstraintEncoderFactory {
 
-    public OntologyZ3BitVectorConstraintEncoderFactory(
-            Lattice lattice,
-            Context context,
+    public OntologyZ3BitVectorConstraintEncoderFactory(Lattice lattice, Context context,
             Z3BitVectorFormatTranslator z3BitVectorFormatTranslator) {
         super(lattice, context, z3BitVectorFormatTranslator);
     }
 
     @Override
     public Z3BitVectorSubtypeConstraintEncoder createSubtypeConstraintEncoder() {
-        return new OntologyZ3BitVectorSubtypeConstraintEncoder(
-                lattice, context, z3BitVectorFormatTranslator);
+        return new OntologyZ3BitVectorSubtypeConstraintEncoder(lattice, context, formatTranslator);
     }
 }
