cecfd0da9522ed4eea3cdb3c2504f96c02a0d66c
Jeff Luo <j36luo@uwaterloo.ca>
2018-06-18 20:25:11 -0400

    simplify build script and pascali-setup

--- pascali-setup.sh
+++ pascali-setup.sh
@@ -13,16 +13,6 @@ export REPO_SITE="${REPO_SITE:-opprop}"
 
 echo "------ Downloading everthing from REPO_SITE: $REPO_SITE ------"
 
-##### Fetching checker-framework
-if [ -d $JSR308/checker-framework ] ; then
-    (cd $JSR308/checker-framework && git pull)
-else
-    (cd $JSR308 && git clone --depth 1 https://github.com/"$REPO_SITE"/checker-framework.git)
-fi
-
-## build checker-framework, with pre-built jdk
-(cd $JSR308/checker-framework && ./.travis-build-without-test.sh downloadjdk)
-
 ##### build checker-framework-inference
 if [ -d $JSR308/checker-framework-inference ] ; then
     (cd $JSR308/checker-framework-inference && git pull)
@@ -30,7 +20,7 @@ else
     (cd $JSR308 && git clone -b ontology_debug --depth 1 https://github.com/"$REPO_SITE"/checker-framework-inference.git)
 fi
 
-(cd $JSR308/checker-framework-inference && gradle dist)
+(cd $JSR308/checker-framework-inference && ./.travis-build-without-test.sh)
 
 ## Fetching DLJC
 if [ -d $JSR308/do-like-javac ] ; then
