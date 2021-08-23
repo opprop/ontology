42021fd028d10b998fbb085b3b7fb81119f5122f
zcai <z44cai@uwaterloo.ca>
2021-08-23 13:50:56 -0400

    try cloning related repositories in setup

--- dependency-setup.sh
+++ dependency-setup.sh
@@ -8,17 +8,22 @@ WORKING_DIR=$(cd $(dirname "$0") && pwd)
 
 # export SHELLOPTS
 
+if [ -d "/tmp/plume-scripts" ] ; then
+  git -C /tmp/plume-scripts pull -q
+else
+  git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
+fi
+
 #default value is opprop. REPO_SITE may be set to other value for travis test purpose.
 export REPO_SITE="${REPO_SITE:-opprop}"
 
-echo "------ Downloading everthing from REPO_SITE: $REPO_SITE ------"
+echo "------ Downloading everything from REPO_SITE: $REPO_SITE ------"
 
 ##### build checker-framework-inference
 if [ -d $JSR308/checker-framework-inference ] ; then
     (cd $JSR308/checker-framework-inference && git pull)
 else
-    BRANCH=master
-    (cd $JSR308 && git clone -b $BRANCH --depth 1 https://github.com/"$REPO_SITE"/checker-framework-inference.git)
+    /tmp/plume-scripts/git-clone-related $REPO_SITE checker-framework-inference $JSR308/checker-framework-inference
 fi
 
 (cd $JSR308/checker-framework-inference && ./.ci-build-without-test.sh)
@@ -27,8 +32,7 @@ fi
 if [ -d $JSR308/do-like-javac ] ; then
     (cd $JSR308/do-like-javac && git pull)
 else
-    BRANCH=master
-    (cd $JSR308 && git clone -b $BRANCH --depth 1 https://github.com/"$REPO_SITE"/do-like-javac.git)
+    /tmp/plume-scripts/git-clone-related $REPO_SITE do-like-javac $JSR308/do-like-javac
 fi
 
 ##### build ontology without testing
