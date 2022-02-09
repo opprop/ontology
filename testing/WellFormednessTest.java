e1f6bc912bdef745c38121fe02ede322aaa2f43a
zcai <z44cai@uwaterloo.ca>
2022-02-09 15:23:59 -0500

    add well-formedness test

---
+++ testing/WellFormednessTest.java
@@ -0,0 +1,11 @@
+import ontology.qual.Ontology;
+import ontology.qual.OntologyValue;
+
+public class WellFormednessTest {
+    // :: error: (type.invalid.conflicting.elements)
+    @Ontology(values={OntologyValue.POLY, OntologyValue.FORCE_3D}) String invalidPoly;
+    // :: error: (type.invalid.conflicting.elements)
+    @Ontology(values={OntologyValue.TOP, OntologyValue.FORCE_3D}) String invalidTop;
+    // :: error: (type.invalid.conflicting.elements)
+    @Ontology(values={OntologyValue.BOTTOM, OntologyValue.FORCE_3D}) String invalidBottom;
+}
