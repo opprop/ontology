f66452e0367cea5ae2a7dd16bdb89181999b32e7
zcai <z44cai@uwaterloo.ca>
2022-02-09 14:41:25 -0500

    add well-formedness check for ontology values

--- src/ontology/OntologyVisitor.java
+++ src/ontology/OntologyVisitor.java
@@ -1,8 +1,17 @@
 package ontology;
 
 import checkers.inference.InferenceChecker;
+import checkers.inference.InferenceValidator;
 import checkers.inference.InferenceVisitor;
+import com.sun.source.tree.Tree;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
+import org.checkerframework.common.basetype.BaseTypeChecker;
+import org.checkerframework.framework.source.DiagMessage;
+import org.checkerframework.framework.type.AnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.framework.type.QualifierHierarchy;
+
+import java.util.List;
 
 public class OntologyVisitor extends InferenceVisitor<OntologyChecker, BaseAnnotatedTypeFactory> {
 
@@ -12,5 +21,10 @@ public class OntologyVisitor extends InferenceVisitor<OntologyChecker, BaseAnnot
             BaseAnnotatedTypeFactory factory,
             boolean infer) {
         super(checker, ichecker, factory, infer);
+    }
+
+    @Override
+    protected InferenceValidator createTypeValidator() {
+        return new OntologyTypeValidator(checker, this, atypeFactory);
     }
 }
