0d34a9cb8e749e32ceca356c822120d4df46f972
zcai <z44cai@uwaterloo.ca>
2022-02-09 15:25:15 -0500

    fix formatting

--- src/ontology/OntologyTypeValidator.java
+++ src/ontology/OntologyTypeValidator.java
@@ -2,6 +2,11 @@ package ontology;
 
 import checkers.inference.InferenceValidator;
 import checkers.inference.InferenceVisitor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.lang.model.element.AnnotationMirror;
+import javax.tools.Diagnostic;
 import ontology.qual.Ontology;
 import ontology.qual.OntologyValue;
 import ontology.util.OntologyUtils;
@@ -11,32 +16,24 @@ import org.checkerframework.framework.type.AnnotatedTypeFactory;
 import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.framework.type.QualifierHierarchy;
 
-import javax.lang.model.element.AnnotationMirror;
-import javax.tools.Diagnostic;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 /**
- * This class checks the well-formedness of ontology values used inside an
- * {@link Ontology} annotation. The current rules include:
- * 1. If the type is TOP, BOTTOM, or POLY, the annotation shouldn't contain
- *  other values (e.g., @Ontology({POLY, FORCE_3D}) is invalid).
+ * This class checks the well-formedness of ontology values used inside an {@link Ontology}
+ * annotation. The current rules include: 1. If the type is TOP, BOTTOM, or POLY, the annotation
+ * shouldn't contain other values (e.g., @Ontology({POLY, FORCE_3D}) is invalid).
  */
 public class OntologyTypeValidator extends InferenceValidator {
     public OntologyTypeValidator(
             BaseTypeChecker checker,
             InferenceVisitor<?, ?> visitor,
-            AnnotatedTypeFactory atypeFactory
-    ) {
+            AnnotatedTypeFactory atypeFactory) {
         super(checker, visitor, atypeFactory);
     }
 
     @Override
     protected List<DiagMessage> isTopLevelValidType(
-            QualifierHierarchy qualifierHierarchy, AnnotatedTypeMirror type
-    ) {
-        List<DiagMessage> errorMsgs = new ArrayList<>(super.isTopLevelValidType(qualifierHierarchy, type));
+            QualifierHierarchy qualifierHierarchy, AnnotatedTypeMirror type) {
+        List<DiagMessage> errorMsgs =
+                new ArrayList<>(super.isTopLevelValidType(qualifierHierarchy, type));
 
         AnnotationMirror am = type.getAnnotation(Ontology.class);
         if (am != null) {
@@ -45,9 +42,14 @@ public class OntologyTypeValidator extends InferenceValidator {
                 if (values.contains(OntologyValue.POLY)
                         || values.contains(OntologyValue.TOP)
                         || values.contains(OntologyValue.BOTTOM)) {
-                    // Should only have one ontology value when the type is one of {POLY, TOP, BOTTOM}
-                    errorMsgs.add(new DiagMessage(
-                            Diagnostic.Kind.ERROR, "type.invalid.conflicting.elements", am, type));
+                    // Should only have one ontology value when the type is one of {POLY, TOP,
+                    // BOTTOM}
+                    errorMsgs.add(
+                            new DiagMessage(
+                                    Diagnostic.Kind.ERROR,
+                                    "type.invalid.conflicting.elements",
+                                    am,
+                                    type));
                 }
             }
         }
--- src/ontology/OntologyVisitor.java
+++ src/ontology/OntologyVisitor.java
@@ -3,15 +3,7 @@ package ontology;
 import checkers.inference.InferenceChecker;
 import checkers.inference.InferenceValidator;
 import checkers.inference.InferenceVisitor;
-import com.sun.source.tree.Tree;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
-import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.framework.source.DiagMessage;
-import org.checkerframework.framework.type.AnnotatedTypeFactory;
-import org.checkerframework.framework.type.AnnotatedTypeMirror;
-import org.checkerframework.framework.type.QualifierHierarchy;
-
-import java.util.List;
 
 public class OntologyVisitor extends InferenceVisitor<OntologyChecker, BaseAnnotatedTypeFactory> {
 
