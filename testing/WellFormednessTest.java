9c40fa7b77d189c29f258395a4e7d62896b3e754
Werner Dietl <wdietl@gmail.com>
2023-02-28 19:02:27 -0500

    Apply more spotless formatting

--- testing/WellFormednessTest.java
+++ testing/WellFormednessTest.java
@@ -3,9 +3,9 @@ import ontology.qual.OntologyValue;
 
 public class WellFormednessTest {
     // :: error: (type.invalid.conflicting.elements)
-    @Ontology(values={OntologyValue.POLY, OntologyValue.FORCE_3D}) String invalidPoly;
+    @Ontology(values = {OntologyValue.POLY, OntologyValue.FORCE_3D}) String invalidPoly;
     // :: error: (type.invalid.conflicting.elements)
-    @Ontology(values={OntologyValue.TOP, OntologyValue.FORCE_3D}) String invalidTop;
+    @Ontology(values = {OntologyValue.TOP, OntologyValue.FORCE_3D}) String invalidTop;
     // :: error: (type.invalid.conflicting.elements)
-    @Ontology(values={OntologyValue.BOTTOM, OntologyValue.FORCE_3D}) String invalidBottom;
+    @Ontology(values = {OntologyValue.BOTTOM, OntologyValue.FORCE_3D}) String invalidBottom;
 }
